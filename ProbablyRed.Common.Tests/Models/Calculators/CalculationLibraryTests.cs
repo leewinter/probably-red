02bc7353877c1fe9ef4cb297bec67236fc74ef57
Lee Winter <lee.winter@comgroupltd.com>
2023-05-18 18:10:34 +0100

    feat(api): Add CalculationLibraryTest for expected behaviour

---
+++ ProbablyRed.Common.Tests/Models/Calculators/CalculationLibraryTests.cs
@@ -0,0 +1,22 @@
+﻿using ProbablyRed.Common.Models.Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbablyRed.Common.Tests.Models.Calculators
+{
+    [TestClass]
+    public class CalculationLibraryTests
+    {
+        [TestMethod]
+        public void GetAvailableCalculators_Get_ShouldReturnAsExpected()
+        {
+            var availableCalculators = CalculationLibrary.GetAvailableCalculators();
+
+            Assert.IsNotNull(availableCalculators);
+            Assert.IsTrue(availableCalculators.Any());
+        }
+    }
+}
