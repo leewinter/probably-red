345c94c550428b3c63fbe2fbf4c5ee3b93ffe471
Lee Winter <lee.winter@comgroupltd.com>
2023-05-18 18:02:08 +0100

    feat(api): Included CalculationDeserializer Tests

---
+++ ProbablyRed.Common.Tests/Models/Calculators/CalculationDeserializerTests.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using ProbablyRed.Common.Models.Calculators;
+using ProbablyRed.Common.Models.Calculators.Calculations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbablyRed.Common.Tests.Models.Calculators
+{
+    [TestClass]
+    public class CalculationDeserializerTests
+    {
+        [TestMethod]
+        public void Deserialize_NormalInput_ShouldSucceed()
+        {
+            var expectedCalculator = new CombinedProbability() { ProbabilityA = 0.25m, ProbabilityB = 0.25m };
+            var expectedBody = JsonConvert.SerializeObject(expectedCalculator);
+            dynamic payload = JsonConvert.DeserializeObject<dynamic>(expectedBody);
+
+            var result = CalculationDeserializer.Deserialize(payload) as CombinedProbability;
+
+            Assert.AreEqual(expectedCalculator.ProbabilityA, result.ProbabilityA);
+            Assert.AreEqual(expectedCalculator.ProbabilityB, result.ProbabilityB);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "Unable to identify StrategyCalculator CalculationType during deserialization. Ensure a valid payload was provided")]
+        public void Deserialize_UnexpectedInput_ShouldThrowArgumentNullException()
+        {
+            var unexpectedInput = new CalculationResult() { };
+            var expectedBody = JsonConvert.SerializeObject(unexpectedInput);
+            dynamic payload = JsonConvert.DeserializeObject<dynamic>(expectedBody);
+
+            CalculationDeserializer.Deserialize(payload);
+        }
+    }
+}
--- ProbablyRed.Common/Models/Calculators/CalculationDeserializer.cs
+++ ProbablyRed.Common/Models/Calculators/CalculationDeserializer.cs
@@ -7,12 +7,23 @@ namespace ProbablyRed.Common.Models.Calculators
     {
         public static IStrategyCalculator Deserialize(dynamic calc)
         {
-            dynamic data = JsonConvert.DeserializeObject<dynamic>(calc.ToString());
-            var calculationType = data.CalculationType.ToString();
-            Assembly assem = typeof(IStrategyCalculator).Assembly;
-            var type = assem.GetType(calculationType);
-            IStrategyCalculator calculator = JsonConvert.DeserializeObject(calc.ToString(), type);
-            return calculator;
+            try
+            {
+                dynamic data = JsonConvert.DeserializeObject<dynamic>(calc.ToString());
+                var calculationType = data.CalculationType.ToString();
+                Assembly assem = typeof(IStrategyCalculator).Assembly;
+                var type = assem.GetType(calculationType);
+                IStrategyCalculator calculator = JsonConvert.DeserializeObject(calc.ToString(), type);
+                return calculator;
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.StartsWith("Cannot perform runtime binding on a null reference")) {
+                    throw new ArgumentNullException("Unable to identify StrategyCalculator CalculationType during deserialization. Ensure a valid payload was provided", ex);
+                }
+                
+                throw;
+            }            
         }
     }
 }
